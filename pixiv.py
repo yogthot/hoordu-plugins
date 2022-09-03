f5b29922fb28c825aa7d6d6300a45566cb66ee89
yogthot <yogthot@myon.moe>
2022-09-03 17:35:33 +0100

    pixiv: added new url format for href

--- pixiv.py
+++ pixiv.py
@@ -332,7 +332,11 @@ class Pixiv(SimplePluginBase):
         if re.match('^https?:\/\/\S+$', href):
             return href
         
-        if href.startswith('/'):
+        if href.startswith('//'):
+            base_url = re.match('^[^:]+:', page_url).group(0)
+            return base_url + href
+            
+        elif href.startswith('/'):
             base_url = re.match('^[^:]+:\/\/[^\/]+', page_url).group(0)
             return base_url + href
         
